f5b3e85ae33c6a5ce320e68850bbb06841f8a3a6
Alejandro Paton <alejandropatonaraujo2@gmail.com>
2024-04-09 15:23:48 +0000

    Correcting error in section 2

--- src/Controller/PizzonController.php
+++ src/Controller/PizzonController.php
@@ -19,17 +19,17 @@ class PizzonController extends AbstractController{
 
    #[Route('/pizzon/section1', name:'pizzon_section1')]
    public function pizzonSection1() : Response{
-       return $this->render('@Pizzon/section1.html.twig');
+       return $this->render('Pizzon/section1.html.twig');
    }
   
    #[Route('/pizzon/section2', name:'pizzon_section2')]
    public function pizzonSection2() : Response{
-       return $this->render('@Pizzon/section2.html.twig');
+       return $this->render('Pizzon/section2.html.twig');
    }
 
 
    #[Route('/pizzon/section3', name:'pizzon_section3')]
    public function pizzonSection3() : Response{
-       return $this->render('@Pizzon/section3.html.twig');
+       return $this->render('Pizzon/section3.html.twig');
    }
 }
